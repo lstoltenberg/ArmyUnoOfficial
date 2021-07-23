057e8030ce40318b1b41a4171b66f3fc32279220
davon-davis <davond009@gmail.com>
2021-07-23 10:13:48 -0500

    add start game styles

--- frontend/you-kno-frontend/src/App.tsx
+++ frontend/you-kno-frontend/src/App.tsx
@@ -1,24 +1,54 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Button, Input } from "@material-ui/core";
+import { Button, Input, makeStyles, TextField } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  startButton: {
+    textAlign: "center",
+    backgroundColor: "#A9AEB1",
+    width: "490px",
+    height: "112px",
+    borderRadius: "10px",
+    fontWeight: "bold",
+    fontSize: "40px",
+    lineHeight: "47px",
+    display: "flex",
+    alignItems: "center",
+    color: "#000000",
+  },
+
+  playerNameField: {
+    width: "237px",
+    height: "24px",
+    background: "#E6E6E6",
+    //marginBottom: "16px",
+    margin: theme.spacing(1),
+    padding: "0",
+  },
+}));
 
 function App() {
+  const styles = useStyles();
   return (
     <div className="App">
       <header className="App-header">
-        <Input />
-        <Input />
-        <Input />
-        <Input />
-        <Input />
-        <Input />
-        <Input />
-        <Input />
-        <Input />
-        <Input />
+        <TextField
+          variant="outlined"
+          size="small"
+          className={styles.playerNameField}
+        />
+        <TextField variant="outlined" className={styles.playerNameField} />
+        <TextField variant="outlined" className={styles.playerNameField} />
+        <TextField variant="outlined" className={styles.playerNameField} />
+        <TextField variant="outlined" className={styles.playerNameField} />
+        <TextField variant="outlined" className={styles.playerNameField} />
+        <TextField variant="outlined" className={styles.playerNameField} />
+        <TextField variant="outlined" className={styles.playerNameField} />
+        <TextField variant="outlined" className={styles.playerNameField} />
+        <TextField variant="outlined" className={styles.playerNameField} />
 
-        <Button>Start Game</Button>
+        <Button className={styles.startButton}>Start Game</Button>
       </header>
     </div>
   );
